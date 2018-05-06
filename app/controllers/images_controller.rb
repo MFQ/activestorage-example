899052e3539b33563631b42c1a216e7b1a9161a4
Muhammad Fazil Qureshi <softmfq@gmail.com>
2018-05-06 18:13:15 +0500

    direct upload

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -61,6 +61,10 @@ class ImagesController < ApplicationController
     end
   end
 
+  def direct_upload
+    @image = Image.new
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_image
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :images
+  resources :images do
+    collection do
+      get 'direct_upload'
+    end
+  end
   root to: 'images#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
